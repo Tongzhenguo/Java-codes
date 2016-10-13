1cf0aa1ae3dc4e7a47782ed15e4ade3e62e37cfb
Tongzhenguo <18210872892@163.com>
2016-10-13 12:22:43 +0800

    验证数独我的想法

--- src/main/java/OJ/ValidSudoku.java
+++ src/main/java/OJ/ValidSudoku.java
@@ -1,5 +1,9 @@
 package OJ;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
 /**
  * Created by Administrator on 2016/10/11.
  *
@@ -7,7 +11,7 @@ package OJ;
  * The Sudoku board（9*9） could be partially filled, where empty cells are filled with the character '.'.
  */
 public class ValidSudoku {
-    public boolean isValidSudoku(char[][] board) {
+/*    public boolean isValidSudoku(char[][] board) {
         //每一行每一列每一宫都有数字且不重复 由此设置三个数组来判断
         boolean [][]row = new boolean[9][9];
         boolean [][]col = new boolean[9][9];
@@ -24,5 +28,52 @@ public class ValidSudoku {
             }
         }
         return true;
+    }*/
+    /**
+     * 有一种解法：
+     *     1，将遍历到的数据存到向量中，如果出现过就返回
+     *     2，表示小九宫格可以用Map（i/3-j/3,Vector）存储
+     *         i/3“-”j/3标识唯一一个九宫格
+     */
+    public boolean isValidSudoku(char[][] board) {
+        // 存储行列以及小九宫格的数字
+        Vector<Integer> row = new Vector<Integer>(9);
+        Vector<Integer> col = new Vector<Integer>(9);
+        String sign;
+        Map<String,Vector<Integer>> zone = new HashMap<String, Vector<Integer>>(9){{
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    put(i/3+"-"+j/3,new Vector<Integer>(9));
+                }
+            }
+
+        }};
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                int num= board[i][j] - '0';
+                sign = i/3 + "-" +j/3;
+                if(row.contains(num) || col.contains(num) || zone.get(sign).contains(num)){
+                    return false;
+                }
+                if(num!='.'){
+                    row.add(board[i][j]-'0');
+                    col.add(board[j][i]-'0');
+                    zone.get(sign).add(board[i][j]-'0');
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ValidSudoku sudu = new ValidSudoku();
+        char[][] chars = new char[9][9];
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++) {
+                chars[i][j] = (char)('0'+i+1);
+            }
+            }
+
+        System.out.println(sudu.isValidSudoku(chars));
     }
 }
