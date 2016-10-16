c651ed335deb3062cb5e832caa1e8ab741e85448
Tongzhenguo <18210872892@163.com>
2016-10-16 10:48:53 +0800

    链表和二叉树

---
+++ src/main/java/datastructure/ListNode.java
@@ -0,0 +1,11 @@
+package datastructure;
+
+/**
+ * Created by YYT on 2016/10/9.
+ *  Definition for singly-linked list.
+ */
+public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
---
+++ src/main/java/datastructure/TreeNode.java
@@ -0,0 +1,11 @@
+package datastructure;
+
+/**
+ * Created by Administrator on 2016/10/16.
+ */
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
