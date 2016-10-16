feed7c624da84b30858cbc6e3320521fd72e7b25
Tongzhenguo <18210872892@163.com>
2016-10-16 11:01:11 +0800

    11111111111

--- src/main/java/datastructure/TreeNode.java
+++ src/main/java/datastructure/TreeNode.java
@@ -4,8 +4,8 @@ package datastructure;
  * Created by Administrator on 2016/10/16.
  */
 public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode(int x) { val = x; }
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int x) { val = x; }
   }
