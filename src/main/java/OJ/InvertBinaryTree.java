1348cd20b4f3f7068232318589cb1d06c13ad7e2
Tongzhenguo <18210872892@163.com>
2016-10-19 18:18:11 +0800

    交换二叉树

--- src/main/java/OJ/InvertBinaryTree.java
+++ src/main/java/OJ/InvertBinaryTree.java
@@ -1,5 +1,7 @@
 package OJ;
 
+import datastructure.TreeNode;
+
 /**
  * Created by YYT on 2016/10/19.
  *   4
@@ -17,6 +19,32 @@ package OJ;
  简单讲就是将树的所有分支，左右互换
  */
 public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        /**
+         * 网上看到的方法
+         */
+        if(root == null ){
+            return root;
+        }
+        if(root.left!=null && root.right!=null) {
+            TreeNode t = root.left;
+            root.left = root.right;
+            root.right = t;
+        }
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
 
+    public static void main(String[] args) {
+        TreeNode rt = new TreeNode(442);
+        TreeNode left = new TreeNode(443);
+        TreeNode right = new TreeNode(444);
+        TreeNode node = new TreeNode(445);
+        rt.left = left;
+        rt.right = right;
+        rt.left.left = node;
 
+        System.out.println(new InvertBinaryTree().invertTree(rt).right.left.val);
+    }
 }
