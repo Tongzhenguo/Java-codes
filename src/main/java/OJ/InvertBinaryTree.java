0b1521ed85eaabf5077bc19bb4328ace141ae276
Tongzhenguo <18210872892@163.com>
2016-10-19 14:31:34 +0800

    交换二叉树

---
+++ src/main/java/OJ/InvertBinaryTree.java
@@ -0,0 +1,21 @@
+package OJ;
+
+/**
+ * Created by YYT on 2016/10/19.
+ *   4
+ /   \
+ 2     7
+ / \   / \
+ 1   3 6   9
+ to
+ 4
+ /   \
+ 7     2
+ / \   / \
+ 9   6 3   1
+
+ 简单讲就是将树的所有分支，左右互换
+ */
+public class InvertBinaryTree {
+
+}
