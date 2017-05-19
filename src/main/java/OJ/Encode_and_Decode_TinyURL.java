a63f5b4a0f0b927d528f1ab273093624f7dfb04a
Tongzhenguo <18210872892@163.com>
2017-05-19 14:19:34 +0800

    四个数组和的个数

---
+++ src/main/java/OJ/Encode_and_Decode_TinyURL.java
@@ -0,0 +1,26 @@
+package OJ;
+
+/**
+ * Created by Tongzhenguo on 2017/5/19.
+ TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk.
+
+ Design the encode and decode methods for the TinyURL service.
+ There is no restriction on how your encode/decode algorithm should work.
+ You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+ 设计URL和tiny URL的编解码算法
+ *
+ */
+/*public class Encode_and_Decode_TinyURL {
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+
+    }
+
+
+}*/
---
+++ src/main/java/OJ/FourSum_II.java
@@ -0,0 +1,54 @@
+package OJ;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Tongzhenguo on 2017/5/19.
+ *
+ Given four lists A, B, C, D of integer values, compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+　A,B,C,D四个整型数组，数组长度相等，设计算法使得A[i] + B[j] + C[k] + D[l]＝０，求满足条件的(i, j, k, l)的个数
+ To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500. All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 231 - 1.
+
+ Example:
+
+ Input:
+ A = [ 1, 2]
+ B = [-2,-1]
+ C = [-1, 2]
+ D = [ 0, 2]
+
+ Output:
+ 2
+
+ Explanation:
+ The two tuples are:
+ 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+ 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+ *
+ */
+public class FourSum_II {
+
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        //记录目标和与对应的总数
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<C.length; i++) {
+            for(int j=0; j<D.length; j++) {
+                int sum = C[i] + D[j];
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+
+        int res=0;
+        for(int i=0; i<A.length; i++) {
+            for(int j=0; j<B.length; j++) {
+                res += map.getOrDefault(-1 * (A[i]+B[j]), 0);
+            }
+        }
+
+        return res;
+    }
+
+
+}
