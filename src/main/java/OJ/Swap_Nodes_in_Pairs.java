9d5760f9b1760a30b123437fcf9354aa261632af
Tongzhenguo <18210872892@163.com>
2017-06-02 14:04:34 +0800

    奇数偶数分组 单链表相邻交换

---
+++ src/main/java/OJ/Swap_Nodes_in_Pairs.java
@@ -0,0 +1,30 @@
+package OJ;
+
+/**
+ * Created by Tongzhenguo on 2017/6/2.
+ * Given a linked list, swap every two adjacent nodes and return its head.
+
+ For example,
+ Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+ Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+ *
+ */
+public class Swap_Nodes_in_Pairs {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+        while (current.next != null && current.next.next != null) {
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+            first.next = second.next;
+            current.next = second;
+            current.next.next = first;
+            current = current.next.next;
+        }
+        return dummy.next;
+    }
+
+
+}
