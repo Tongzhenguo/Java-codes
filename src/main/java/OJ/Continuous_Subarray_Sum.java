ab1ea8aa31458a517667fd9d3d8826d48e0d8a80
Tongzhenguo <18210872892@163.com>
2017-06-16 14:23:08 +0800

    数学问题:找到k倍数的连续数组；最大化除法的结果

---
+++ src/main/java/OJ/Continuous_Subarray_Sum.java
@@ -0,0 +1,42 @@
+package OJ;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Tongzhenguo on 2017/6/16.
+ * Given a list of non-negative numbers and a target integer k,
+ * write a function to check if the array has a continuous subarray of size at least 2 that sums up to the multiple of k, that is, sums up to n*k where n is also an integer.
+
+ Example 1:
+ Input: [23, 2, 4, 6, 7],  k=6
+ Output: True
+ Explanation: Because [2, 4] is a continuous subarray of size 2 and sums up to 6.
+ Example 2:
+ Input: [23, 2, 6, 4, 7],  k=6
+ Output: True
+ Explanation: Because [23, 2, 6, 4, 7] is an continuous subarray of size 5 and sums up to 42.
+ *
+ */
+public class Continuous_Subarray_Sum {
+    /**
+     * We iterate through the input array exactly once, keeping track of the running sum mod k of the elements in the process.
+     * If we find that a running sum value at index j has been previously seen before in some earlier index i in the array,
+     * then we know that the sub-array (i,j] contains a desired sum.
+     */
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>(){{put(0,-1);}};;
+        int runningSum = 0;
+        for (int i=0;i<nums.length;i++) {
+            runningSum += nums[i];
+            if (k != 0) runningSum %= k;
+            Integer prev = map.get(runningSum);
+            if (prev != null) {
+                if (i - prev > 1) return true;
+            }
+            else map.put(runningSum, i);
+        }
+        return false;
+    }
+
+}
--- src/main/java/OJ/Optimal_Division.java
+++ src/main/java/OJ/Optimal_Division.java
@@ -3,8 +3,10 @@ package OJ;
 /**
  * Created by Tongzhenguo on 2017/6/15.
  * Given a list of positive integers, the adjacent integers will perform the float division. For example, [2,3,4] -> 2 / 3 / 4.
+ Elements in the given array will be in range [2, 1000].
 
- However, you can add any number of parenthesis at any position to change the priority of operations. You should find out how to add parenthesis to get the maximum result, and return the corresponding expression in string format. Your expression should NOT contain redundant parenthesis.
+ However, you can add any number of parenthesis at any position to change the priority of operations.
+ You should find out how to add parenthesis to get the maximum result, and return the corresponding expression in string format. Your expression should NOT contain redundant parenthesis.
 
  Example:
  Input: [1000,100,10,2]
@@ -22,14 +24,34 @@ package OJ;
  Note:
 
  The length of the input array is [1, 10].
- Elements in the given array will be in range [2, 1000].
  There is only one optimal division for each test case.
  */
 public class Optimal_Division {
+ /**
+  * Using some simple math we can find the easy solution of this problem.
+  * Consider the input in the form of [a,b,c,d], now we have to set priority of operations to maximize a/b/c/d.
+  * We know that to maximize fraction p/qp/q, qq(denominator) should be minimized. So, to maximize a/b/c/d we have to first minimize b/c/d.
+  * Now our objective turns to minimize the expression b/c/d.
 
-//    public String optimalDivision(int[] nums) {
-//
-//    }
+  There are two possible combinations of this expression, b/(c/d) and (b/c)/d.
+
+  b/(c/d):(b/c)/d = b/c/d =>d/c :　1/(d*c)
+  Obviously, d/c > 1/(d*c) for d>1
+  *
+  */
+    public String optimalDivision(int[] nums) {
+     if (nums.length == 1)
+      return nums[0] + "";
+     if (nums.length == 2)
+      return nums[0] + "/" + nums[1];
+     StringBuilder res = new StringBuilder(nums[0] + "/(" + nums[1]);
+     for (int i = 2; i < nums.length; i++) {
+      res.append("/" + nums[i]);
+     }
+     res.append(")");
+     return res.toString();
+
+    }
 
 
 
