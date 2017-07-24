241bf344571b87f43401900e59f63bfa0967b5c0
Tongzhenguo <18210872892@163.com>
2017-07-24 14:10:37 +0800

    树问题:返回最下层最左元素，赋值next节点，构造BST

--- src/main/java/OJ/Find_Bottom_Left_Tree_Value.java
+++ src/main/java/OJ/Find_Bottom_Left_Tree_Value.java
@@ -1,5 +1,10 @@
 package OJ;
 
+import datastructure.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by Tongzhenguo on 2017/7/21.
  * Given a binary tree, find the leftmost value in the last row of the tree.
@@ -32,4 +37,22 @@ package OJ;
  *
  */
 public class Find_Bottom_Left_Tree_Value {
+      /**
+       * Doing BFS right-to-left means we can simply return the last node's value and don't have to keep track of the first node in the current row or even care about rows at all.
+       * Inspired by @fallcreek's solution (not published) which uses two nested loops to go row by row but already had the right-to-left idea making it easier.
+       * I just took that further.
+       */
+      public int findBottomLeftValue(TreeNode root) {
+           Queue<TreeNode> queue = new LinkedList<>();
+           queue.add(root);
+           while (!queue.isEmpty()) {
+            root = queue.poll();
+            if (root.right != null)
+             queue.add(root.right);
+            if (root.left != null)
+             queue.add(root.left);
+           }
+           return root.val;
+      }
+
 }
--- src/main/java/OJ/Unique_Binary_Search_Trees_II.java
+++ src/main/java/OJ/Unique_Binary_Search_Trees_II.java
@@ -20,55 +20,43 @@ import java.util.List;
  2     1         2                 3
  */
 public class Unique_Binary_Search_Trees_II {
-
-    public List<TreeNode> generateTrees(int n) {
-
-        return genTrees(1,n);
-    }
-
     /**
-     * I start by noting that 1..n is the in-order traversal for any BST with nodes 1 to n.
-     * So if I pick i-th node as my root, the left subtree will contain elements 1 to (i-1), and the right subtree will contain elements (i+1) to n.
-     * I use recursive calls to get back all possible trees for left and right subtrees and combine them in all possible ways with the root.
+     * result[i] stores the result until length i.
+     * For the result for length i+1, select the root node j from 0 to i, combine the result from left side and right side.
+     * Note for the right side we have to clone the nodes as the value will be offsetted by j.
      */
-    public List<TreeNode> genTrees (int start, int end) {
-
-        List<TreeNode> list = new ArrayList<TreeNode>();
-
-        if(start>end) {
-//            list.add(null);
-            return list;
+    public static List<TreeNode> generateTrees(int n) {
+        List<TreeNode>[] result = new List[n + 1];
+        result[0] = new ArrayList<TreeNode>();
+        if (n == 0) {
+            return result[0];
         }
 
-        if(start == end){
-            list.add(new TreeNode(start));
-            return list;
-        }
-
-        List<TreeNode> left,right;
-        for(int i=start;i<=end;i++) {
-
-            left = genTrees(start, i-1);
-            right = genTrees(i+1,end);
-
-            for(TreeNode lnode: left) {
-                for(TreeNode rnode: right) {
-                    TreeNode root = new TreeNode(i);
-                    root.left = lnode;
-                    root.right = rnode;
-                    list.add(root);
+        result[0].add(null);
+        for (int len = 1; len <= n; len++) {
+            result[len] = new ArrayList<TreeNode>();
+            for (int j = 0; j < len; j++) {
+                for (TreeNode nodeL : result[j]) {
+                    for (TreeNode nodeR : result[len - j - 1]) {
+                        TreeNode node = new TreeNode(j + 1);
+                        node.left = nodeL;
+                        node.right = clone(nodeR, j + 1);
+                        result[len].add(node);
+                    }
                 }
             }
-
         }
-
-        return list;
+        return result[n];
     }
-//    Input:
-//            3
-//    Output:
-//            [[2,1,3]]
-//    Expected:
-//            [[1,null,2,null,3],[1,null,3,2],[2,1,3],[3,1,null,null,2],[3,2,null,1]]
+
+    private static TreeNode clone(TreeNode n, int offset) {
+        if (n == null) {
+            return null;
+        }
+        TreeNode node = new TreeNode(n.val + offset);
+        node.left = clone(n.left, offset);
+        node.right = clone(n.right, offset);
+        return node;
+    }
 
 }
