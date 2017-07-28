9b7666d6c46a7400b86d0bcb1151912b80ad75b1
Tongzhenguo <tongzhenguotongzhenguo@gmail.com>
2017-07-28 14:06:51 +0800

    DFS:找到所有的递增序列

---
+++ src/main/java/OJ/Increasing_Subsequences.java
@@ -0,0 +1,43 @@
+package OJ;
+
+/**
+ * Created by Administrator on 2017/7/28.
+ *
+ * Given an integer array, your task is to find all the different possible increasing subsequences of the given array, and the length of an increasing subsequence should be at least 2 .
+
+ Example:
+ Input: [4, 6, 7, 7]
+ Output: [[4, 6], [4, 7], [4, 6, 7], [4, 6, 7, 7], [6, 7], [6, 7, 7], [7,7], [4,7,7]]
+ Note:
+ The length of the given array will not exceed 15.
+ The range of integer in the given array is [-100,100].
+ The given array may contain duplicates, and two equal integers should also be considered as a special case of increasing sequence.
+ *
+ */
+public class Increasing_Subsequences {
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        Set<List<Integer>> res= new HashSet<List<Integer>>();
+        List<Integer> holder = new ArrayList<Integer>();
+        findSequence(res, holder, 0, nums);
+        List result = new ArrayList(res);
+        return result;
+    }
+
+    public void findSequence(Set<List<Integer>> res, List<Integer> holder, int index, int[] nums) {
+        if (holder.size() >= 2) {
+            res.add(new ArrayList(holder));
+        }
+        for (int i = index; i < nums.length; i++) {
+            if(holder.size() == 0 || holder.get(holder.size() - 1) <= nums[i]) {
+                holder.add(nums[i]);
+                findSequence(res, holder, i + 1, nums);
+                holder.remove(holder.size() - 1);
+            }
+        }
+    }
+
+
+}
+
+
+
