5daea31860720e2213361b80a5ba0b9a09dc0400
Tongzhenguo <tongzhenguotongzhenguo@gmail.com>
2017-08-01 14:08:33 +0800

    DFS:朋友圈个数，距离矩阵

---
+++ src/main/java/OJ/zero1_Matrix.java
@@ -0,0 +1,73 @@
+package OJ;
+
+import java.util.Queue;
+
+/**
+ * Created by arachis on 2017/8/1.
+ * Given a matrix consists of 0 and 1, find the distance of the nearest 0 for each cell.
+
+ The distance between two adjacent cells is 1.
+ Example 1:
+ Input:
+
+ 0 0 0
+ 0 1 0
+ 0 0 0
+ Output:
+ 0 0 0
+ 0 1 0
+ 0 0 0
+ Example 2:
+ Input:
+
+ 0 0 0
+ 0 1 0
+ 1 1 1
+ Output:
+ 0 0 0
+ 0 1 0
+ 1 2 1
+ *
+ */
+public class zero1_Matrix {
+    /*
+    * General idea is BFS. Some small tricks:
+        At beginning, set cell value to Integer.MAX_VALUE if it is not 0.
+        If newly calculated distance >= current distance, then we don't need to explore that cell again.
+    * */
+
+    public int[][] updateMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    queue.offer(new int[] {i, j});
+                }
+                else {
+                    matrix[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            for (int[] d : dirs) {
+                int r = cell[0] + d[0];
+                int c = cell[1] + d[1];
+                if (r < 0 || r >= m || c < 0 || c >= n ||
+                        matrix[r][c] <= matrix[cell[0]][cell[1]] + 1) continue;
+                queue.add(new int[] {r, c});
+                matrix[r][c] = matrix[cell[0]][cell[1]] + 1;
+            }
+        }
+
+        return matrix;
+    }
+
+
+}
