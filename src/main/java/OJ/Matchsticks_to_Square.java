30f34959c0fc167f57b712fecc579d4a560f02a8
Tongzhenguo <tongzhenguotongzhenguo@gmail.com>
2017-08-03 14:09:42 +0800

    DFS:编码解码，拼正方形，太平洋大西洋水流

---
+++ src/main/java/OJ/Matchsticks_to_Square.java
@@ -0,0 +1,28 @@
+package OJ;
+
+/**
+ * Created by arachis on 2017/8/3.
+ * 火柴杆:matchsticks
+ *
+ * Remember the story of Little Match Girl? By now, you know exactly what matchsticks the little match girl has,
+ * please find out a way you can make one square by using up all those matchsticks.
+ * You should not break any stick, but you can link them up, and each matchstick must be used exactly one time.
+
+ Your input will be several matchsticks the girl has, represented with their stick length.
+ Your output will either be true or false, to represent whether you could make one square using all the matchsticks the little match girl has.
+
+ Example 1:
+ Input: [1,1,2,2,2]
+ Output: true
+ Explanation: You can form a square with length 2, one side of the square came two sticks with length 1.
+
+ Example 2:
+ Input: [3,3,3,3,4]
+ Output: false
+ Explanation: You cannot find a way to form a square with all the matchsticks.
+ */
+public class Matchsticks_to_Square {
+
+
+
+}
---
+++ src/main/java/OJ/Pacific_Atlantic_Water_Flow.java
@@ -0,0 +1,97 @@
+package OJ;
+
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by arachis on 2017/8/2.
+ * Given an m x n matrix of non-negative integers representing the height of each unit cell in a continent,
+ * the "Pacific ocean" touches the left and top edges of the matrix and the "Atlantic ocean" touches the right and bottom edges.
+
+ Water can only flow in four directions (up, down, left, or right) from a cell to another one with height equal or lower.
+
+ Find the list of grid coordinates where water can flow to both the Pacific and Atlantic ocean.
+
+ Note:
+ The order of returned grid coordinates does not matter.
+ Both m and n are less than 150.
+ Example:
+
+ Given the following 5x5 matrix:
+
+ Pacific ~   ~   ~   ~   ~
+ ~  1   2   2   3  (5) *
+ ~  3   2   3  (4) (4) *
+ ~  2   4  (5)  3   1  *
+ ~ (6) (7)  1   4   5  *
+ ~ (5)  1   1   2   4  *
+ *   *   *   *   * Atlantic
+
+ Return:
+
+ [[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]] (positions with parentheses in above matrix).
+ */
+public class Pacific_Atlantic_Water_Flow {
+
+    /**
+     * 1.Two Queue and add all the Pacific border to one queue; Atlantic border to another queue.
+       2.Keep a visited matrix for each queue. In the end, add the cell visited by two queue to the result.
+     */
+    int[][]dir = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+
+    public List<int[]> pacificAtlantic(int[][] matrix) {
+        List<int[]> res = new LinkedList<>();
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return res;
+        }
+        int n = matrix.length, m = matrix[0].length;
+        //One visited map for each ocean
+        boolean[][] pacific = new boolean[n][m];
+        boolean[][] atlantic = new boolean[n][m];
+        Queue<int[]> pQueue = new LinkedList<>();
+        Queue<int[]> aQueue = new LinkedList<>();
+        for(int i=0; i<n; i++){ //Vertical border
+            pQueue.offer(new int[]{i, 0});
+            aQueue.offer(new int[]{i, m-1});
+            pacific[i][0] = true;
+            atlantic[i][m-1] = true;
+        }
+        for(int i=0; i<m; i++){ //Horizontal border
+            pQueue.offer(new int[]{0, i});
+            aQueue.offer(new int[]{n-1, i});
+            pacific[0][i] = true;
+            atlantic[n-1][i] = true;
+        }
+        bfs(matrix, pQueue, pacific);
+        bfs(matrix, aQueue, atlantic);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(pacific[i][j] && atlantic[i][j])
+                    res.add(new int[]{i,j});
+            }
+        }
+        return res;
+    }
+
+    /**
+     * Water flood from ocean to the cell. Since water can only flow from high/equal cell to low cell,
+     *   add the neighboor cell with height larger or equal to current cell to the queue and mark as visited.
+     */
+    public void bfs(int[][]matrix, Queue<int[]> queue, boolean[][]visited){
+        int n = matrix.length, m = matrix[0].length;
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+            for(int[] d:dir){
+                int x = cur[0]+d[0];
+                int y = cur[1]+d[1];
+                if(x<0 || x>=n || y<0 || y>=m || visited[x][y] || matrix[x][y] < matrix[cur[0]][cur[1]]){
+                    continue;
+                }
+                visited[x][y] = true;
+                queue.offer(new int[]{x, y});
+            }
+        }
+    }
+
+
+}
