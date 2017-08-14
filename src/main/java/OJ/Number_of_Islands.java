3537fc6deb91bd86607a9577f5644bd78fb6ff4c
Tongzhenguo <tongzhenguotongzhenguo@gmail.com>
2017-08-14 14:08:54 +0800

    DFS:小岛个数，扫雷

---
+++ src/main/java/OJ/Number_of_Islands.java
@@ -0,0 +1,55 @@
+package OJ;
+
+/**
+ * Created by arachis on 2017/8/14.
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+ * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+ * You may assume all four edges of the grid are all surrounded by water.
+
+ Example 1:
+
+ 11110
+ 11010
+ 11000
+ 00000
+ Answer: 1
+
+ Example 2:
+
+ 11000
+ 11000
+ 00100
+ 00011
+ Answer: 3
+ *
+ */
+public class Number_of_Islands {
+
+    private int n;
+    private int m;
+
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        n = grid.length;
+        if (n == 0) return 0;
+        m = grid[0].length;
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++)
+                if (grid[i][j] == '1') {
+                    DFSMarking(grid, i, j);
+                    ++count;
+                }
+        }
+        return count;
+    }
+
+    private void DFSMarking(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
+        grid[i][j] = '0';
+        DFSMarking(grid, i + 1, j);
+        DFSMarking(grid, i - 1, j);
+        DFSMarking(grid, i, j + 1);
+        DFSMarking(grid, i, j - 1);
+    }
+
+}
--- src/main/java/OJ/Shopping_Offers.java
+++ src/main/java/OJ/Shopping_Offers.java
@@ -40,38 +40,38 @@ public class Shopping_Offers {
        Pick whichever is minimum.
        */
       public int shoppingOffers(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
-       int result = Integer.MAX_VALUE;
-       //apply each offer to the needs, and recurse
-       for(int i = 0; i < special.size(); i++) {
-        List<Integer> offer = special.get(i);
-        boolean invalidOffer = false;
-        int offerCount = Integer.MAX_VALUE; // number of times offer can be applied
-        for(int j = 0; j < needs.size(); j++) { // pre-compute number of times offer can be called
-         int remain = needs.get(j) - offer.get(j);
-         if(!invalidOffer && remain < 0) invalidOffer = true; // if offer has more items than needs
-         if(offer.get(j) > 0)
-          offerCount = Math.min(offerCount, needs.get(j)/offer.get(j));
-        }
-        for(int j = 0; j < needs.size(); j++) { // subtract offer items from needs
-         int remain = needs.get(j) - offer.get(j) * offerCount;
-         needs.set(j, remain);
-        }
-        if(!invalidOffer) { //if valid offer, add offer price and recurse remaining needs
-         result = Math.min(result, shoppingOffers(price, special, needs) + (offerCount * offer.get(needs.size())));
-        }
+            int result = Integer.MAX_VALUE;
+            //apply each offer to the needs, and recurse
+            for(int i = 0; i < special.size(); i++) {
+                  List<Integer> offer = special.get(i);
+                  boolean invalidOffer = false;
+                  int offerCount = Integer.MAX_VALUE; // number of times offer can be applied
+                  for(int j = 0; j < needs.size(); j++) { // pre-compute number of times offer can be called
+                      int remain = needs.get(j) - offer.get(j);
+                      if(!invalidOffer && remain < 0) invalidOffer = true; // if offer has more items than needs
+                      if(offer.get(j) > 0)
+                         offerCount = Math.min(offerCount, needs.get(j)/offer.get(j));
+                  }
+             for(int j = 0; j < needs.size(); j++) { // subtract offer items from needs
+                  int remain = needs.get(j) - offer.get(j) * offerCount;
+                  needs.set(j, remain);
+             }
+             if(!invalidOffer) { //if valid offer, add offer price and recurse remaining needs
+                 result = Math.min(result, shoppingOffers(price, special, needs) + (offerCount * offer.get(needs.size())));
+             }
 
-        for(int j = 0; j < needs.size(); j++) { // reset the needs
-         int remain = needs.get(j) + offer.get(j) * offerCount;
-         needs.set(j, remain);
-        }
-       }
+             for(int j = 0; j < needs.size(); j++) { // reset the needs
+                  int remain = needs.get(j) + offer.get(j) * offerCount;
+                  needs.set(j, remain);
+             }
+            }
 
-       // choose b/w offer and non offer
-       int nonOfferPrice = 0;
-       for(int i = 0; i < needs.size(); i++) {
-        nonOfferPrice += price.get(i) * needs.get(i);
-       }
-       return Math.min(result, nonOfferPrice);
+            // choose b/w offer and non offer
+            int nonOfferPrice = 0;
+            for(int i = 0; i < needs.size(); i++) {
+                  nonOfferPrice += price.get(i) * needs.get(i);
+            }
+            return Math.min(result, nonOfferPrice);
       }
 
 }
